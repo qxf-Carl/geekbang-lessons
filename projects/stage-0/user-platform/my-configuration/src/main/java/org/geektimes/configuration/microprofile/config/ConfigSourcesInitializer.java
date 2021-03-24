d8bce4d10723285d6547b9b12fe08c50e0fcdcf5
qingyi.qxf <quxiaofeng@hundunyun.com.cn>
2021-03-25 00:10:26 +0800

    update README.md

---
+++ projects/stage-0/user-platform/my-configuration/src/main/java/org/geektimes/configuration/microprofile/config/ConfigSourcesInitializer.java
@@ -0,0 +1,34 @@
+package org.geektimes.configuration.microprofile.config;
+
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.ConfigBuilder;
+import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
+import org.geektimes.configuration.microprofile.config.source.servlet.ServletContextConfigSource;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Set;
+
+public class ConfigSourcesInitializer implements ServletContainerInitializer {
+
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext servletContext) throws ServletException {
+        // 获取当前 ClassLoader
+        ClassLoader classLoader = servletContext.getClassLoader();
+        ConfigProviderResolver configProviderResolver = ConfigProviderResolver.instance();
+        ConfigBuilder configBuilder = configProviderResolver.getBuilder();
+        // 配置 ClassLoader
+        configBuilder.forClassLoader(classLoader);
+        // 默认配置源（内建的，静态的）
+        configBuilder.addDefaultSources();
+        // 通过发现配置源（动态的）
+        configBuilder.addDiscoveredConverters();
+        // 增加扩展配置源（基于 Servlet 引擎）
+//        configBuilder.withSources(servletContextConfigSource);
+        // 获取 Config
+        Config config = configBuilder.build();
+        // 注册 Config 关联到当前 ClassLoader
+        configProviderResolver.registerConfig(config, classLoader);
+    }
+}

ef30c1b42d12239acef82f0bbe93a2e7ec17884a
qingyi.qxf <quxiaofeng@hundunyun.com.cn>
2021-03-31 16:55:16 +0800

    polish postInvocation & demo

--- projects/stage-0/user-platform/my-rest-client/src/test/java/org/geektimes/rest/demo/RestClientDemo.java
+++ projects/stage-0/user-platform/my-rest-client/src/test/java/org/geektimes/rest/demo/RestClientDemo.java
@@ -2,20 +2,42 @@ package org.geektimes.rest.demo;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class RestClientDemo {
 
     public static void main(String[] args) {
+        doGet();
+        doPost();
+    }
+
+    private static void doGet() {
         Client client = ClientBuilder.newClient();
         Response response = client
-                .target("http://127.0.0.1:8080/hello/world")      // WebTarget
+                .target("http://www.baidu.com")      // WebTarget
                 .request() // Invocation.Builder
                 .get();                                     //  Response
 
         String content = response.readEntity(String.class);
 
-        System.out.println(content);
-
+        System.out.println("Get content:\n" + content);
     }
+
+
+    private static void doPost() {
+        Client client = ClientBuilder.newClient();
+
+        Form form = new Form();
+        form.param("name", "qingyi");
+        Response response = client
+                .target("http://www.baidu.com")      // WebTarget
+                .request() // Invocation.Builder
+                .post(Entity.entity(form, MediaType.MULTIPART_FORM_DATA_TYPE));                                     //  Response
+        String content = response.readEntity(String.class);
+        System.out.println("Post content:\n" + content);
+    }
+
 }
